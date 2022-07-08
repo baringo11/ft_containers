91412a0cf4e0912d69b22cffe8f8b390efa61622
javi <javi.baringo@gmail.com>
2022-07-08 23:27:37 +0200

    Stack member functions added

--- srcs/Stack/stack.hpp
+++ srcs/Stack/stack.hpp
@@ -40,6 +40,27 @@ namespace ft
 				this->_ctn = src._ctn;
 				return (*this);
 			}
+
+// -----------  MEMBER FUNCTIONS -----------
+
+			bool empty() const { return (this->_c.empty()); }
+
+			size_type size() const { return (this->_c.size()); }
+
+			value_type& top() { return (this->_c.back()); }
+
+			const value_type& top() const { return (this->_c.back()); }
+			
+			void push(const value_type& val)
+			{
+				this->_c.push_back(val);
+			}
+
+			void pop()
+			{
+				this->_c.pop_back();
+			}
+
     };
 }
 
