fa5a6b9c9a6884277e40efeb9f9c2b25451dee77
javi <javi.baringo@gmail.com>
2022-07-09 21:03:36 +0200

    Tree_iterator implemented in map

--- srcs/Map/map.hpp
+++ srcs/Map/map.hpp
@@ -4,26 +4,41 @@
 #include <functional>
 #include <utility>
 
+#include "../utils/reverse_iterator.hpp"
+#include "../utils/enable_if.hpp"
+#include "../utils/is_integral.hpp"
+#include "../utils/lexicographical_compare.hpp"
+#include "../utils/equal.hpp"
+
+#include "tree_iterator.hpp"
+
 namespace ft
 {
 	template<class key, class T, class Compare = std::less<key>, class Alloc = std::allocator<std::pair<const key,T> > >
 	class map
 	{
 		public:
-			typedef key													key_type;
-			typedef T													mapped_type;
-			typedef std::pair<const key_type, mapped_type>				value_type;
-			typedef Compare												key_compare;
-			typedef Alloc												allocator_type;
-			typedef typename allocator_type::reference					reference;
-			typedef typename allocator_type::const_reference			const_reference;
-			typedef typename allocator_type::pointer					pointer;
-			typedef typename allocator_type::const_pointer				const_pointer;
+			typedef key															key_type;
+			typedef T															mapped_type;
+			typedef std::pair<const key_type, mapped_type>						value_type;
+			typedef Compare														key_compare;
+			typedef Alloc														allocator_type;
 
-			// iterarors
+			typedef typename allocator_type::reference							reference;
+			typedef typename allocator_type::const_reference					const_reference;
+			typedef typename allocator_type::pointer							pointer;
+			typedef typename allocator_type::const_pointer						const_pointer;
 
-			typedef std::ptrdiff_t										difference_type;
-			typedef size_t												size_type;
+			typedef ft::node<value_type>										node_type;
+			typedef node_type*													node_ptr;
+
+			typedef ft::tree_iterator<value_type, node_type>					iterator;
+			typedef ft::tree_iterator<const value_type, node_type>				const_iterator;
+			typedef ft::reverse_iterator<iterator>								reverse_iterator;
+			typedef ft::reverse_iterator<const_iterator>						const_reverse_iterator;
+
+			typedef std::ptrdiff_t												difference_type;
+			typedef size_t														size_type;
 
 			class value_compare : public std::binary_function<value_type, value_type, bool>
 			{
