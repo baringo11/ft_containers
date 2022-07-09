c9e46202ff66d86351a4bf2e3cf0698959d6bbc0
javi <javi.baringo@gmail.com>
2022-07-09 19:50:25 +0200

    Starting with Map

--- Makefile
+++ Makefile
@@ -1,17 +1,18 @@
-NAME = container
+NAME = ft_container
 CXX = clang++
-CXXFLAGS = -Werror -Wall -Wextra -std=c++98 -IVector
+CXXFLAGS = -Werror -Wall -Wextra -std=c++98 -IVector -IStack -IMap
 
-INCL = 		srcs/utils/reverse_iterator.hpp \
+INCL = 		srcs/utils/reverse_iterator.hpp srcs/utils/lexicographical_compare.hpp 	srcs/utils/equal.hpp\
 			srcs/utils/enable_if.hpp srcs/utils/is_integral.hpp srcs/utils/iterator_traits.hpp \
-			srcs/Vector/vector.hpp srcs/Vector/vector_iterator.hpp
+			srcs/Vector/vector.hpp srcs/Vector/vector_iterator.hpp \
+			srcs/Stack/stack.hpp \
+			srcs/Map/map.hpp
 
 FILES = srcs/main.cpp
 FT_OBJS = $(addprefix $(OBJ_DIR)/,$(FILES:.cpp=.o_ft))
 STD_OBJS =	$(addprefix $(OBJ_DIR)/,$(FILES:.cpp=.o_std))
 
 OBJ_DIR = obj
-OBJ = $(addprefix $(OBJ_DIR)/,$(FILES:.cpp=.o))
 
 all: $(NAME)
 
---
+++ srcs/Map/map.hpp
@@ -0,0 +1,47 @@
+#ifndef MAP_HPP
+# define MAP_HPP
+
+#include <functional>
+#include <utility>
+
+namespace ft
+{
+	template<class key, class T, class Compare = std::less<key>, class Alloc = std::allocator<std::pair<const key,T>> >
+	class map
+	{
+		public:
+			typedef key													key_type;
+			typedef T													mapped_type;
+			typedef std::pair<const key_type, mapped_type>				value_type;
+			typedef Compare												key_compare;
+			typedef Alloc												allocator_type;
+			typedef typename allocator_type::reference					reference;
+			typedef typename allocator_type::const_reference			const_reference;
+			typedef typename allocator_type::pointer					pointer;
+			typedef typename allocator_type::const_pointer				const_pointer;
+
+			// iterarors
+
+			typedef std::ptrdiff_t										difference_type;
+			typedef size_t												size_type;
+
+			class value_compare : public std::binary_function<value_type, value_type, bool>
+			{
+				protected:
+					Compare comp;
+					value_compare (Compare c) : comp(c) {}  // constructed with map's comparison object
+				public:
+					typedef bool result_type;
+					typedef value_type first_argument_type;
+					typedef value_type second_argument_type;
+					bool operator() (const value_type& x, const value_type& y) const
+					{
+						return comp(x.first, y.first);
+					}
+			};
+			
+
+	};
+}
+
+#endif
